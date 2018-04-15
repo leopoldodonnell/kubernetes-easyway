8e900b369337adab8e0bce984869bea14057220a
Leo ODonnell <leopold.odonnell@gmail.com>
2018-04-14 23:40:06 -0400

    Added missing output for google

---
+++ template/gcloud/output.erb.tf
@@ -0,0 +1,19 @@
+output "cluster_name" {
+  value = "${google_container_cluster.primary.name}"
+}
+
+output "primary_zone" {
+  value = "${google_container_cluster.primary.zone}"
+}
+
+output "additional_zones" {
+  value = "${google_container_cluster.primary.additional_zones}"
+}
+
+output "endpoint" {
+  value = "${google_container_cluster.primary.endpoint}"
+}
+
+output "node_version" {
+  value = "${google_container_cluster.primary.node_version}"
+}
